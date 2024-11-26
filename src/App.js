250c13ea9e5356c946ba9682e48ca5e118d4fd65
unknown <phuc>
2024-11-26 11:03:05 +0700

    login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/home';
+import Login from './pages/login';
 // import AboutPage from './pages/AboutPage';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
