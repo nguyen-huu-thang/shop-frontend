2dbbf7a9858cc81ec0991168ce795693d9023b60
unknown <phuc>
2025-01-09 23:19:49 +0700

    sua trang home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Navbar from '../components/navbar';
 import Banner from '../components/banner';
-import { products } from '../product';
-import ProductCart from '../components/productcart';
 import ConnectWidget from '../components/connectwidget';
 import Footer from '../components/footer';
 import Suggest from '../components/home/suggest';
@@ -28,11 +26,6 @@ function Home() {
         </div>
       </div>
       <BestSell />
-      <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 m-20'>
-        {products.map((product, key) => (
-          <ProductCart key={key} data={product} />
-        ))}
-      </div>
       <div className="flex justify-center items-center mt-3 mb-3 relative">
         <div className="flex items-center justify-center relative px-5 py-2 text-orange-600 font-bold w-full">
             <div
